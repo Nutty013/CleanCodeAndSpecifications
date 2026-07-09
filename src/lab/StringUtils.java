71d2719949e1b2f9ab9eee66847f945dc580c94e
Nutty013 <nutcha.kal@ku.th>
2026-07-09 17:00:53 +0700

    Add files via upload

--- src/lab/StringUtils.java
+++ src/lab/StringUtils.java
@@ -9,9 +9,20 @@ public class StringUtils {
      * @throws IllegalArgumentException ถ้า text เป็น null
      */
     public static int countVowels(String text) {
-        // TODO: ถ้า text เป็น null ให้ throw IllegalArgumentException
-        // TODO: วนนับสระ a,e,i,o,u (พิมพ์เล็ก/ใหญ่) แล้ว return
-        //       (ใช้ตัวแปร local เท่านั้น = pure function)
-        return 0;
+        if (text == null) {
+        throw new IllegalArgumentException("text must not be null");
+    }
+
+    int vowelCount = 0;
+
+    for (int i = 0; i < text.length(); i++) {
+        char c = Character.toLowerCase(text.charAt(i));
+
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            vowelCount++;
+        }
+    }
+
+    return vowelCount;
     }
 }
